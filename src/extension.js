86d6844cdac107012c754a7f0edf73fe698132d2
Daniel Rincón <danielrinconher@gmail.com>
2023-03-11 12:16:48 -0500

    Adding icons

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,50 @@
 const vscode = require('vscode')
+const fs = require('fs')
+const path = require('path')
+const { generateTheme } = require('./icons/themes')
+const files = require('./icons/files/_fileSchema.json')
+const folders = require('./icons/folders/_folderSchema.json')
 
 function activate(context) {
-  console.log('Congratulations, your extension "r-icons" is now active!')
+  const base = generateTheme()
+
+  const finalTheme = {
+    // Definitions
+    fonts: base.fonts,
+    iconDefinitions: {
+      ...base.iconDefinitions,
+      ...files.iconDefinitions,
+      ...folders.iconDefinitions,
+    },
+
+    // Files
+    file: files.file ?? base.file,
+    fileNames: { ...base.fileNames, ...files.fileNames },
+    fileExtensions: { ...base.fileExtensions, ...files.fileExtensions },
+    languageIds: { ...base.languageIds, ...files.languageIds },
+
+    // Folders
+    folder: folders.folder ?? base.folder,
+    folderExpanded: folders.folderExpanded ?? base.folderExpanded,
+    folderNames: { ...base.folderNames, ...folders.folderNames },
+    folderNamesExpanded: {
+      ...base.folderNamesExpanded,
+      ...folders.folderNamesExpanded,
+    },
+
+    // Misc
+    light: { ...base.light, ...folders.light, ...files.light },
+    highContrast: {
+      ...base.highContrast,
+      ...folders.highContrast,
+      ...files.highContrast,
+    },
+  }
+
+  fs.writeFileSync(
+    path.join(__dirname, './r-icons-icon-theme.json'),
+    JSON.stringify(finalTheme)
+  )
 
   let disposable = vscode.commands.registerCommand(
     'r-icons.helloWorld',
---
+++ src/icons/themes/index.js
@@ -0,0 +1,28 @@
+const seti = require('./seti/_setiSchema.json')
+
+function generateTheme() {
+  return {
+    // Definitions
+    fonts: [...seti.fonts],
+    iconDefinitions: { ...seti.iconDefinitions },
+
+    // Files
+    file: seti.file,
+    fileNames: { ...seti.fileNames },
+    fileExtensions: { ...seti.fileExtensions },
+    languageIds: { ...seti.languageIds },
+
+    // Folders
+    folder: null,
+    folderExpanded: null,
+    folderNames: {},
+
+    // Misc
+    light: { ...seti.light },
+    highContrast: { ...seti.highContrast },
+  }
+}
+
+module.exports = {
+  generateTheme,
+}
